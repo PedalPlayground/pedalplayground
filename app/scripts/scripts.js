4aa04fd625f65dd7db4a7f392e282f6797694f2a
Travis Lehman <tlehman@webstaurantstore.com>
2017-02-21 22:49:58 -0500

    Got json data working, images kinda

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     // Run function to ajax scope data
-	GetScopeData();
+	GetPedalData();
 
 	var multiplier = "15";
 
@@ -20,7 +20,8 @@ $(document).ready(function(){
 		var selected  = $('#add-pedal').find(":selected");
 		var width  	  = $(selected).data("width") * multiplier;
 		var height    = $(selected).data("height") * multiplier;
-		var pedal     = '<div class="pedal" style="width:'+width+'px;height:'+height+'px;" />';
+		var image  	  = $(selected).data("image");
+		var pedal     = '<div class="pedal" style="width:'+width+'px;height:'+height+'px; background-image:url('+image+')" />';
 
 		$('.pedalboard').append(pedal);
 		$('.pedal').draggabilly({
@@ -29,30 +30,57 @@ $(document).ready(function(){
 		return false;
 	});
 
-	window.GetPedalData = function(){
-		$.ajax({
-			url: "resources/data/scopes.json",
-			dataType: 'text',
-			type: "GET",
-			success: function(data){
-				data = $.parseJSON(data.replace(/\r\n/g, "").replace(/\t/g, ""));
-				var scopes = [];
-				for(var scope in data){
-					scopes.push( new Scope(
-						data[scope].Name || "",
-						data[scope].Type || "",
-						data[scope].Manufacturer || "",
-						data[scope].Description || "",
-						data[scope].Icon || "",
-						data[scope].Zoom || "",
-						data[scope].Notes || "",
-						data[scope].Image || [],
-						data[scope].Stats || []
-					));
-				}
-				RenderPedals(pedals);
-			}
-		});
-	};
 
 });
+
+window.Pedal = function( type, brand, name, width, height, image ){
+	this.Type = type || "";
+	this.Brand = brand || "";
+	this.Name = name || "";
+	this.Width = width || "";
+	this.Height = height || "";
+	this.Image = image || "";
+}
+
+window.GetPedalData = function(){
+	console.log('GetPedalData');
+	$.ajax({
+		url: "public/data/pedals.json",
+		dataType: 'text',
+		type: "GET",
+		success: function(data){
+			data = $.parseJSON(data.replace(/\r\n/g, "").replace(/\t/g, ""));
+			var pedals = [];
+			for(var pedal in data){
+				pedals.push( new Pedal(
+					data[pedal].Type 	|| "",
+					data[pedal].Brand 	|| "",
+					data[pedal].Name 	|| "",
+					data[pedal].Width 	|| "",
+					data[pedal].Height 	|| "",
+					data[pedal].Image 	|| ""
+				));
+			}
+			RenderPedals(pedals);
+			console.log("ajax success");
+		}
+	});
+};
+
+window.RenderPedals = function(pedals){
+	console.log('RenderPedals');
+	for(var i in pedals) {
+		// <option id="bd-2" data-width="2.75" data-height="5">Blues Driver</option>
+		// <option id="dd-500" data-width="6.75" data-height="5.44">DD-500</option>
+		// <option id="fv-30" data-width="3.15" data-height="7.56">FV-30L</option>
+
+		var $pedal = $("<option>"+ pedals[i].Brand + " " + pedals[i].Name +"</option>").attr('id', pedals[i].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, ''));
+
+		$pedal.data('width', pedals[i].Width);
+		$pedal.data('height', pedals[i].Height);
+		$pedal.data('height', pedals[i].Height);
+		$pedal.data('image', pedals[i].Image);
+
+		$pedal.appendTo('.pedal-list');
+	}
+}
--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="icon" href="favicon.ico">
-	<link rel="stylesheet" href="site-prod/stylesheets/styles.css">
-	<script type="text/javascript" src="site-prod/scripts/scripts.js"></script>
+	<link rel="stylesheet" href="public/stylesheets/styles.css">
+	<script type="text/javascript" src="public/scripts/scripts.js"></script>
 </head>
 <!-- !Body -->
 <body>
@@ -12,8 +12,9 @@
 		<div class="sidebar__section">
 			<h4>Add a Pedal</h4>
 			<form id="add-pedal">
-				<select>
-					<optgroup label="BOSS">
+				<select class="pedal-list">
+					<option id="bd-2" data-width="2.75" data-height="5" data-image="public/images/pedals/strymon-timeline.png">Blues Driver</option>
+					<?php /*<optgroup label="BOSS">
 						<option id="bd-2" data-width="2.75" data-height="5">Blues Driver</option>
 						<option id="dd-500" data-width="6.75" data-height="5.44">DD-500</option>
 						<option id="fv-30" data-width="3.15" data-height="7.56">FV-30L</option>
@@ -22,6 +23,7 @@
 						<option id="timeline" data-width="6.75" data-height="5">Timeline</option>
 						<option id="blue-sky" data-width="3.5" data-height="5.4">Blue Sky</option>
 					</optgroup>
+					*/ ?>
 				</select>
 				<button>Add Pedal</button>
 			</form>
