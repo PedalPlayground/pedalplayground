eb02f3922b7045a0efa2349e3c162137f63ecdd3
Matthew McDermott <matthewmcd07@gmail.com>
2021-12-08 08:51:50 +1300

    Formatted EvEmitter

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -11420,110 +11420,127 @@
 
 /* jshint unused: true, undef: true, strict: true */
 
-( function( global, factory ) {
-  // universal module definition
-  /* jshint strict: false */ /* globals define, module, window */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD - RequireJS
-    define( 'ev-emitter/ev-emitter',factory );
-  } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS - Browserify, Webpack
-    module.exports = factory();
-  } else {
-    // Browser globals
-    global.EvEmitter = factory();
-  }
+(
+	function(global, factory) {
+		// universal module definition
+		/* jshint strict: false */ /* globals define, module, window */
+		if (typeof define == 'function' && define.amd) {
+			// AMD - RequireJS
+			define('ev-emitter/ev-emitter',factory);
+		} 
+		
+		else if (typeof module == 'object' && module.exports) {
+			// CommonJS - Browserify, Webpack
+			module.exports = factory();
+		} 
+		
+		else {
+			// Browser globals
+			global.EvEmitter = factory();
+		}
+	}(
+		typeof window != 'undefined' ? window: this, function() {
+			function EvEmitter() {}
 
-}( typeof window != 'undefined' ? window : this, function() {
+			var proto = EvEmitter.prototype;
 
+			proto.on = function(eventName, listener) {
+				if (!eventName || !listener) {
+					return;
+				}
 
+				// set events hash
+				var events = this._events = this._events || {};
 
-function EvEmitter() {}
+				// set listeners array
+				var listeners = events[eventName] = events[eventName] || [];
 
-var proto = EvEmitter.prototype;
+				// only add once
+				if (listeners.indexOf(listener) == -1) {
+					listeners.push(listener);
+				}
 
-proto.on = function( eventName, listener ) {
-  if ( !eventName || !listener ) {
-    return;
-  }
-  // set events hash
-  var events = this._events = this._events || {};
-  // set listeners array
-  var listeners = events[ eventName ] = events[ eventName ] || [];
-  // only add once
-  if ( listeners.indexOf( listener ) == -1 ) {
-    listeners.push( listener );
-  }
+				return this;
+			};
 
-  return this;
-};
+			proto.once = function(eventName, listener) {
+				if (!eventName || !listener) {
+					return;
+				}
+				// add event
+				this.on(eventName, listener);
 
-proto.once = function( eventName, listener ) {
-  if ( !eventName || !listener ) {
-    return;
-  }
-  // add event
-  this.on( eventName, listener );
-  // set once flag
-  // set onceEvents hash
-  var onceEvents = this._onceEvents = this._onceEvents || {};
-  // set onceListeners object
-  var onceListeners = onceEvents[ eventName ] = onceEvents[ eventName ] || {};
-  // set flag
-  onceListeners[ listener ] = true;
+				// set once flag
+				// set onceEvents hash
+				var onceEvents = this._onceEvents = this._onceEvents || {};
 
-  return this;
-};
+				// set onceListeners object
+				var onceListeners = onceEvents[eventName] = onceEvents[eventName] || {};
 
-proto.off = function( eventName, listener ) {
-  var listeners = this._events && this._events[ eventName ];
-  if ( !listeners || !listeners.length ) {
-    return;
-  }
-  var index = listeners.indexOf( listener );
-  if ( index != -1 ) {
-    listeners.splice( index, 1 );
-  }
+				// set flag
+				onceListeners[listener] = true;
 
-  return this;
-};
+				return this;
+			};
 
-proto.emitEvent = function( eventName, args ) {
-  var listeners = this._events && this._events[ eventName ];
-  if ( !listeners || !listeners.length ) {
-    return;
-  }
-  // copy over to avoid interference if .off() in listener
-  listeners = listeners.slice(0);
-  args = args || [];
-  // once stuff
-  var onceListeners = this._onceEvents && this._onceEvents[ eventName ];
+			proto.off = function(eventName, listener) {
+				var listeners = this._events && this._events[eventName];
 
-  for ( var i=0; i < listeners.length; i++ ) {
-    var listener = listeners[i]
-    var isOnce = onceListeners && onceListeners[ listener ];
-    if ( isOnce ) {
-      // remove listener
-      // remove before trigger to prevent recursion
-      this.off( eventName, listener );
-      // unset once flag
-      delete onceListeners[ listener ];
-    }
-    // trigger listener
-    listener.apply( this, args );
-  }
+				if (!listeners || !listeners.length) {
+					return;
+				}
 
-  return this;
-};
+				var index = listeners.indexOf(listener);
+				if (index != -1) {
+					listeners.splice(index, 1);
+				}
 
-proto.allOff = function() {
-  delete this._events;
-  delete this._onceEvents;
-};
+				return this;
+			};
 
-return EvEmitter;
+			proto.emitEvent = function(eventName, args) {
+				var listeners = this._events && this._events[eventName];
+				if (!listeners || !listeners.length) {
+					return;
+				}
 
-}));
+				// copy over to avoid interference if .off() in listener
+				listeners = listeners.slice(0);
+				args = args || [];
+
+				// once stuff
+				var onceListeners = this._onceEvents && this._onceEvents[eventName];
+
+				for (var i=0; i < listeners.length; i++) {
+					var listener = listeners[i]
+					var isOnce = onceListeners && onceListeners[listener];
+
+					if (isOnce) {
+						// remove listener
+						// remove before trigger to prevent recursion
+						this.off(eventName, listener);
+
+						// unset once flag
+						delete onceListeners[listener];
+					}
+
+					// trigger listener
+					listener.apply(this, args);
+				}
+
+				return this;
+			};
+
+			proto.allOff = function() {
+				delete this._events;
+				delete this._onceEvents;
+			};
+
+			return EvEmitter;	
+		}
+	)
+);
+
 
 /*!
  * Unipointer v2.3.0
