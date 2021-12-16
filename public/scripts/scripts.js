b28cf44d52d5f1334fd9246deca14c8c4b652423
Matthew McDermott <matthewmcd07@gmail.com>
2021-12-17 10:14:44 +1300

    Started formatting Bootstrap v3.4.1

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -12650,16 +12650,17 @@
  */
 
 if (typeof jQuery === 'undefined') {
-  throw new Error('Bootstrap\'s JavaScript requires jQuery')
+	throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
 +function ($) {
-  'use strict';
-  var version = $.fn.jquery.split(' ')[0].split('.')
-  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] > 3)) {
-    throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
-  }
+	'use strict';
+  	var version = $.fn.jquery.split(' ')[0].split('.')
+  	if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] > 3)) {
+  		throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
+ 	}
 }(jQuery);
+
 
 /* ========================================================================
  * Bootstrap: transition.js v3.4.1
